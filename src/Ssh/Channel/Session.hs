41f4842532454fd3757b6bf8380a895de72d5af8
Bart Coppens <kde@bartcoppens.be>
2011-06-17 23:51:03 +0200

    Add handlers to print the result of an exec request on a channel

--- src/Ssh/Channel/Session.hs
+++ src/Ssh/Channel/Session.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+-- | Support for interactive sessions, remote executions, etc. as per SSH Connection Protocol (RFC 4251) Section 6.
 module Ssh.Channel.Session (
       sessionHandler
     , requestExec
@@ -11,34 +12,62 @@ import Data.Binary.Put
 
 import Network.Socket (Socket, SockAddr (..), SocketType (..), socket, connect)
 import qualified Control.Monad.State as MS
+import qualified Data.ByteString.Lazy as B
 
 import Ssh.Packet
 import Ssh.Channel
 import Ssh.String
 import Ssh.Transport
 import Ssh.NetworkIO
+import Ssh.Debug
 
-sessionHandler = ChannelHandler "session" handleSession
+-- | This is the default handler for a session
+sessionHandler = ChannelHandler "session" handleDefaultSession
 
-data SessionData =
-      ExecRequest {
+-- | Different kinds of sessions are supported
+data SessionType =
+    -- | Execute a remote command
+      ExecuteCommand
+
+-- | Different kinds of request can be sent to the server
+data SessionRequest =
+    -- | Execute a remote command with the given string
+    ExecRequest {
         execCommand :: SshString
       }
 
-putSessionData :: SessionData -> Put
+-- | 'Put' for sending a request to the server with its associated payload
+putSessionData :: SessionRequest -> Put
 putSessionData (ExecRequest e) = do
     putString e
 
+-- TODO: verify that this is an open "ssh-connection" channel?
+
+-- | Given the socket, run the specified command remotely on this 'Channel'
 requestExec :: Socket -> SshString -> Channel ChannelInfo
 requestExec socket cmd = do
+    -- Request the "exec" command on this channel
     nr <- getLocalChannelNr
     let requestData = runPut $ putSessionData $ ExecRequest cmd
         request = ChannelRequest nr "exec" True requestData
     MS.lift $ sPutPacket socket request
+
+    -- update the channelinfo with the correct handler function to handle the data of this exec request
     s <- MS.get
-    return s
+    let cih  = channelInfoHandler s
+        cih' = cih { channelHandler = handleExecRequest }
+        ret  = s { channelInfoHandler = cih' }
+    return $ ret
 
-handleSession :: SshString -> Channel ChannelInfo
-handleSession = error "Handle: Session"
 
+-- | Handler for when we haven't sent a request yet
+handleDefaultSession :: SshString -> Channel ChannelInfo
+handleDefaultSession s = error $ "Handle: Session: " ++ (show s)
 
+-- | This 'Channel' has sent an ExecuteCommand request, handle the return data!
+handleExecRequest :: SshString -> Channel ChannelInfo
+handleExecRequest s = do
+    printDebugLifted logDebug "This is the result of an exec request:"
+    let raw = B.unpack s
+    MS.liftIO $ putStr $ map (toEnum . fromEnum) raw
+    MS.get >>= return
