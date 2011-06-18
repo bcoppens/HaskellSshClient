17035ca113196100ebe3447b46f499e0a1507cd9
Bart Coppens <kde@bartcoppens.be>
2011-06-18 16:44:57 +0200

    Handle more Channel messages from the remote side

--- src/Ssh/Channel.hs
+++ src/Ssh/Channel.hs
@@ -23,6 +23,8 @@ import qualified Data.ByteString.Lazy as B
 import qualified Control.Monad.State as MS
 import qualified Data.Map as Map
 
+import Control.Monad
+import Data.Maybe
 import Data.List
 
 import Ssh.Packet
@@ -34,10 +36,13 @@ import Ssh.String
 --   The handler can update the channel information through the 'Channel' State
 data ChannelInfo = ChannelInfo {
       channelLocalId :: Int
-    , channelRemoteId :: Int
+    , channelRemoteId :: Maybe Int              -- ^ Nothing if we have not yet received the OpenConfirmation with the remote ID, or Just remoteId
     , sentEof :: Bool
-    , channelWindowSizeLeft :: Int
-    , channelMaxPacketSize :: Int
+    , gotEof  :: Bool
+    , channelLocalWindowSizeLeft :: Int
+    , channelLocalMaxPacketSize :: Int
+    , channelRemoteWindowSizeLeft :: Maybe Int  -- ^ Nothing if we have not yet received the OpenConfirmation from the remote, or Just windowSizeLeft
+    , channelRemoteMaxPacketSize :: Maybe Int   -- ^ Nothing if we have not yet received the OpenConfirmation from the remote, or Just maxPacketSize
     , channelInfoHandler :: ChannelHandler
 }
 
@@ -53,7 +58,7 @@ data ChannelHandler = ChannelHandler {
 
 -- | Information about a specific channel, so we can keep a mapping of live channels
 data GlobalChannelInfo = GlobalChannelInfo {
-    -- Maps active channels to their 'ChannelInfo' state
+    -- Maps active channels (on their local ID, because the remote will send with the recipientChannel) to their 'ChannelInfo' state
       usedChannels :: Map.Map Int ChannelInfo
     -- Keep a list of free channel IDs
     , freeChannels :: [Int]
@@ -98,10 +103,10 @@ openChannel handler openInfo = do
         free  = take 1 $ freeChannels state
 
     -- Open the channel
-    let remote = 42
-        ws = 2^31
-        ps = 32768
-        chanInfo = ChannelInfo local remote False ws ps handler
+    let remoteId    = Nothing
+        ws          = 2^31
+        ps          = 32768
+        chanInfo    = ChannelInfo local remoteId False False ws ps Nothing Nothing handler
         openRequest = ChannelOpen name local ws ps openInfo
 
     -- Request to open the channel
@@ -113,8 +118,42 @@ openChannel handler openInfo = do
 
     return chanInfo
 
--- | When data comes in for one of our channels, be sure to see to which one it is, and dispatch the payload data to it to update
+-- | Get the 'ChannelInfo' with this local id, if it exists yet
+getChannel :: Int -> Channels (Maybe ChannelInfo)
+getChannel local = do
+    state <- MS.get
+    return $ Map.lookup local (usedChannels state)
+
+-- | Looks up the local channel id, applies the update function to it, and return it. Makes handleChannel somewhat less repetitive to write
+updateInfoWith :: Int -> (ChannelInfo -> ChannelInfo) -> Channels ChannelInfo
+updateInfoWith nr action = do
+    info <- fromJust `liftM` getChannel nr
+    let newInfo = action info
+    modifyUsedChannel nr newInfo
+    return newInfo
+
+-- | Handle a 'Packet' coming to us. Can dispatch the request to a 'Channel's handler, or change the window size of a 'Channel', confirm its opening, closing, etc.
 handleChannel :: Packet -> Channels ChannelInfo
+
+-- The channel was correctly opened. Update the remote information for this channel
+handleChannel (ChannelOpenConfirmation recipientNr senderNr initWS maxPS payload) = do
+    updateInfoWith recipientNr $ \info -> info { channelRemoteId = Just senderNr, channelRemoteWindowSizeLeft = Just initWS, channelRemoteMaxPacketSize = Just maxPS }
+
+-- The remote says we can send some more bytes to this channel
+handleChannel (ChannelWindowAdjust nr toAdd) = do
+    updateInfoWith nr $ \info -> info { channelRemoteWindowSizeLeft = Just $ toAdd + (fromJust $ channelRemoteWindowSizeLeft info) }
+
+-- Remote sends that his side has reached an EOF
+handleChannel (ChannelEof nr) = do
+    updateInfoWith nr $ \info -> info { gotEof = True }
+
+-- Remote closed this channel, remove it from our list for later reuse once we have it removed too
+handleChannel (ChannelClose nr) = do
+    -- TODO
+    info <- fromJust `liftM` getChannel nr
+    return info
+
+-- When data comes in for one of our channels, be sure to see to which one it is, and dispatch the payload data to it to update
 handleChannel (ChannelData nr payload) = do
     state <- MS.get
 
@@ -131,7 +170,8 @@ handleChannel (ChannelData nr payload) = do
             printDebugLifted logWarning $ "No handler found at lookup for channel " ++ show nr
             return $ error "No handler!"
 
--- TODO throw to lower level protocol handling functions!
+-- TODO Handle other cases we don't care about yet
+-- And these messages we don't know about. Throw to lower level protocol handling functions! TODO
 handleChannel p = do
     state <- MS.get
     printDebugLifted logDebugExtended "HandleChannel: ignored packet"
