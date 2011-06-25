130764800957f5a29e6e969e5fd69ff7248830ab
Bart Coppens <kde@bartcoppens.be>
2011-06-25 19:53:20 +0200

    Add length requirements on the key when testing AES

--- tests/Ssh/Cryption/Tests.hs
+++ tests/Ssh/Cryption/Tests.hs
@@ -5,6 +5,7 @@ module Ssh.Cryption.Tests (
 ) where
 
 import Test.Framework
+import Test.QuickCheck
 import qualified Test.HUnit as H
 import Test.Framework.Providers.HUnit
 import Test.Framework.Providers.QuickCheck2
@@ -20,7 +21,7 @@ import Ssh.String
 aesCbcEncryptThenDecrypt ks plain key iv =
     let encrypted = evalState (cbcAesEncrypt ks key plain) $ CryptionInfo iv
         decrypted = evalState (cbcAesDecrypt ks key encrypted) $ CryptionInfo iv
-    in  encrypted == decrypted
+    in  (length key >= 16 && length iv >= 16) ==> encrypted == decrypted
 
 aesEncryptThenDecrypt = map (\ks -> testProperty ("AES " ++ show ks ++ " CBC Encrypt then Decrypt") $ aesCbcEncryptThenDecrypt ks) [ 256 ]
 
