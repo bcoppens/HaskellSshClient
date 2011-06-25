6664d27d1b5bd3b7870a0d6445d25e19b0608196
Bart Coppens <kde@bartcoppens.be>
2011-06-25 15:17:44 +0200

    Add a simple test: check if AES CBC decrypting after encrypting works. Seems to fail...

--- tests/Ssh/Cryption/Tests.hs
+++ tests/Ssh/Cryption/Tests.hs
@@ -7,16 +7,26 @@ module Ssh.Cryption.Tests (
 import Test.Framework
 import qualified Test.HUnit as H
 import Test.Framework.Providers.HUnit
+import Test.Framework.Providers.QuickCheck2
+
+import Data.Word
+import Control.Monad.State
 
 import qualified Data.ByteString.Lazy as B
 
 import Ssh.Cryption
 import Ssh.String
 
+aesCbcEncryptThenDecrypt ks plain key iv =
+    let encrypted = evalState (cbcAesEncrypt ks key plain) $ CryptionInfo iv
+        decrypted = evalState (cbcAesDecrypt ks key encrypted) $ CryptionInfo iv
+    in  encrypted == decrypted
 
+aesEncryptThenDecrypt = map (\ks -> testProperty ("AES " ++ show ks ++ " CBC Encrypt then Decrypt") $ aesCbcEncryptThenDecrypt ks) [ 256 ]
 
 tests :: [Test]
 tests = concat
     [
+        aesEncryptThenDecrypt
     ]
 
--- tests/TestSuite.hs
+++ tests/TestSuite.hs
@@ -3,9 +3,11 @@ module Main where
 import Test.Framework (defaultMain, testGroup)
 
 import qualified Ssh.NetworkIO.Tests
+import qualified Ssh.Cryption.Tests
 
 main :: IO ()
 main = defaultMain
     [
         testGroup "Ssh.NetworkIO.Tests" Ssh.NetworkIO.Tests.tests
+      , testGroup "Ssh.Cryption.Tests" Ssh.Cryption.Tests.tests
     ]
