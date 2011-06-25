42200deb2974aff01fc76cbba83e332b92b78ab2
Bart Coppens <kde@bartcoppens.be>
2011-06-25 14:04:10 +0200

    I'll be adding some tests for the crypto stuff

---
+++ tests/Ssh/Cryption/Tests.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Ssh.Cryption.Tests (
+    tests
+) where
+
+import Test.Framework
+import qualified Test.HUnit as H
+import Test.Framework.Providers.HUnit
+
+import qualified Data.ByteString.Lazy as B
+
+import Ssh.Cryption
+import Ssh.String
+
+
+
+tests :: [Test]
+tests = concat
+    [
+    ]
+
