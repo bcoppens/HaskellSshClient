d3a7363cc1dd77742a54d59cb2c2e091eed85c75
Bart Coppens <kde@bartcoppens.be>
2011-07-03 20:36:59 +0200

    Handy helper function to convert a bytestring to a string

--- src/Ssh/String.hs
+++ src/Ssh/String.hs
@@ -1,9 +1,13 @@
 -- | We use a lazy bytestring everywhere as standard string type
 module Ssh.String (
       SshString
+    , unpackToString
 ) where
 
 import qualified Data.ByteString.Lazy as B
 import Data.ByteString.Lazy.Char8 () -- IsString instance for the above
 
 type SshString = B.ByteString
+
+-- | Convert a ByteString of Word8 to a regular String
+unpackToString s = map (toEnum . fromEnum) $ B.unpack s
